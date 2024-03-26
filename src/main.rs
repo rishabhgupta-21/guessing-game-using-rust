f5f9113cc4d38f988720c102054a9b353bac8e4f
rishabhgupta <contctrishabh@gmail.com>
2024-03-27 02:53:10 +0530

    allowed multiple guesses using loops, and handled invalid input scenarios.

--- src/main.rs
+++ src/main.rs
@@ -7,36 +7,60 @@ use rand::Rng;
 
 fn main() {
     println!("Welcome to the Guessing Game!!!");
+    println!("To exit the game, type 'q' and press Enter.");
 
     // Generating the secret Random Number
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
     
-    println!("Make your guess below (between 1 and 100 inclusive)!");
-    // Creating a variable to store the user's guess
-    let mut guess = String::new();
+    loop {
+        println!("\nMake your guess below! (between 1 and 100 inclusive)");
+        // Creating a variable to store the user's guess
+        let mut guess = String::new();
 
-    // Receiving the user's input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // Receiving the user's input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // Converting the user's guess to an integer-type (Using Shadowing)
-    let guess: u32 = guess.trim().parse().expect("Please input a number between 1 and 100!");
+        // Converting the user's guess to an integer-type (Using Shadowing)
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => {
+                // Validating the user's guess
+                if num < 1 || num > 100 {
+                    println!("Please input a number between 1 and 100!");
+                    continue;
+                }
 
-    // Validating the user's guess
-    if guess < 1 || guess > 100 {
-        println!("Please input a number between 1 and 100!");
-        return;
+                // Store the valid integer guess in the shadowing 'guess' variable
+                num
+            },
+            Err(_) => {
+                // User manually exits the game
+                if guess.trim().to_lowercase() == "q" {
+                    println!("Exiting the game...");
+                    break;
+                }
+
+                // Any other invalid input - continue the game
+                println!("Please input a number between 1 and 100!");
+                continue;
+            }
+        };
+
+        // Displaying the user's guess
+        println!("You guessed: {guess}");
+
+        // Comparing the user's guess to the secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Guess higher ;)"),
+            Ordering::Greater => println!("Guess lower ;)"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 
-    // Displaying the user's guess
-    println!("You guessed: {guess}");
-
-    // Comparing the user's guess to the secret number
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Guess higher ;)"),
-        Ordering::Greater => println!("Guess lower ;)"),
-        Ordering::Equal => println!("You win!"),
-    }
+    println!("Thanks for playing the Guessing Game!");
 }
