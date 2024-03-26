a29239829b87aad1e70be04a6bdebc0c9959df8a
rishabhgupta <contctrishabh@gmail.com>
2024-03-26 21:56:38 +0530

    processing a user's guess.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Bring the 'io' module into scope from the standard library (std)
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Guessing Game!!!");
+    println!("Make your guess below!");
+
+    // Creating a variable to store the user's guess
+    let mut guess = String::new();
+
+    // Receiving the user's input
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
