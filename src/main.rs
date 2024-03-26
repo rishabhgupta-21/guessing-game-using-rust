7786bab637ef2d5f23379eb3d573e8733f670331
rishabhgupta <contctrishabh@gmail.com>
2024-03-26 22:51:25 +0530

    used external create 'rand' to generate the secret random number.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 // Bring the 'io' module into scope from the standard library (std)
 use std::io;
+// The Rng trait defines methods that random number generators implement
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the Guessing Game!!!");
     println!("Make your guess below!");
+
+    // Generating a Random Number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
     // Creating a variable to store the user's guess
     let mut guess = String::new();
@@ -13,5 +19,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Displaying the user's guess
     println!("You guessed: {guess}");
 }
